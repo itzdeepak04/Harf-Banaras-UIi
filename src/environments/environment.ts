94e3846900b9bb4c99a116d6a078763b91ff3541
Deepak <deepaknahak3008@gmail.com>
2026-09-06 17:33:02 +0530

    Fixed env configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ declare var process: any;
 
 export const environment = {
   // react-scripts requires custom environment variables to start with REACT_APP_
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3001'
+  API_URL: process.env.REACT_APP_API_URL || 'https://harf-banaras-api-pink.vercel.app/'
 };
